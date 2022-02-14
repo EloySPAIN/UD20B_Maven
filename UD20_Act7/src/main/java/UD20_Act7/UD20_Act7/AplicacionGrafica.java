e81ad8770c6f1e05a7dcdf093bc3c61732c6a013
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-02-14 12:15:41 +0100

    commitFinal

---
+++ UD20_Act7/src/main/java/UD20_Act7/UD20_Act7/AplicacionGrafica.java
@@ -0,0 +1,90 @@
+package UD20_Act7.UD20_Act7;
+
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class AplicacionGrafica extends JFrame {
+	
+	private JPanel contentPane;
+	private int texto = 0;
+	private JLabel resultado;
+	private JToggleButton btnConversion;
+	private boolean conv = false;
+	
+	public AplicacionGrafica() {
+		
+		setTitle("Calculadora cambio de monedas");
+		
+		setBounds(600, 300, 450, 200);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		setVisible(true);
+		
+		contentPane = new JPanel();
+		
+		contentPane.setLayout(null);
+		
+		setContentPane(contentPane);
+		
+		//Etiqueta1
+		JLabel aConvertir = new JLabel("Cantidad a convertir: ");
+		aConvertir.setBounds(30, 16, 150, 20);
+		contentPane.add(aConvertir);
+		
+		//Campo de texto1
+		JTextField texto1 = new JTextField();
+		texto1.setBounds(155, 16, 60, 25);
+		contentPane.add(texto1);
+		
+		//Etiqueta2
+		JLabel textoRes = new JLabel("Resultado: ");
+		textoRes.setBounds(250, 16, 150, 20);
+		contentPane.add(textoRes);
+		
+		//Etiqueta3
+		resultado = new JLabel(Metodos.calcularEurPts(0));
+		resultado.setBounds(325, 16, 150, 20);
+		contentPane.add(resultado);
+		
+		//Boton Conversión
+		btnConversion = new JToggleButton("Euros Pts");
+		btnConversion.setBounds(60, 90, 120, 20);
+		contentPane.add(btnConversion);
+		btnConversion.addActionListener(new ActionListener(){			
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!conv) {
+					btnConversion.setText("Pts Euros");
+					conv = true;
+				}else {
+					btnConversion.setText("Euros Pts");
+					conv = false;
+				}
+			}
+		});
+		
+		//Boton Calcular
+		JButton btnCalcular = new JButton("Calcular");
+		btnCalcular.setBounds(230, 90, 120, 20);
+		contentPane.add(btnCalcular);
+		btnCalcular.addActionListener(new ActionListener(){			
+			public void actionPerformed(ActionEvent e) {
+			
+				try {
+					if(!conv) {
+						resultado.setText(Metodos.calcularEurPts(Integer.parseInt(texto1.getText())));
+					}else {
+						resultado.setText(Metodos.calcularPtsEur(Integer.parseInt(texto1.getText())));
+					}		
+					
+				}catch(NumberFormatException error){
+					System.out.println("No se ha introducido un número");
+				}		
+			}
+		});
+	}
+}
---
+++ UD20_Act7/src/main/java/UD20_Act7/UD20_Act7/App.java
@@ -0,0 +1,10 @@
+package UD20_Act7.UD20_Act7;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AplicacionGrafica a = new AplicacionGrafica();
+    }
+}
