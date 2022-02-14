e9b88bcc25833cdeddd66eb9a87f336fe0500633
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-02-14 09:57:47 +0100

    Ej1

---
+++ Ej1/src/main/java/Ej1/Ej1/App.java
@@ -0,0 +1,13 @@
+package Ej1.Ej1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
