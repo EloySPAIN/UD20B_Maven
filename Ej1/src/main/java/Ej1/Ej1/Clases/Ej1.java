a02d5087f0b01b90a0a6d077d2a19d45b8969f2b
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-02-14 10:26:21 +0100

    Ej1

--- Ej1/src/main/java/Ej1/Ej1/Clases/Ej1.java
+++ Ej1/src/main/java/Ej1/Ej1/Clases/Ej1.java
@@ -12,14 +12,15 @@ public class Ej1 extends JFrame {
 	private static final long serialVersionUID = 1L;
 	//Creamos las variables
 	private JPanel panel;
-	JTextField texto = new JTextField(10);
-	JComboBox comboBox = new JComboBox<>();
-	JLabel lb = new JLabel("Escribe el titulo de una pelicula");
-	JLabel lb2 = new JLabel("Peliculas");
-	JButton boton = new JButton("Añadir");
+	JTextField x = new JTextField(10);
+	JTextField y = new JTextField(10);
+	JLabel lb = new JLabel("Cambiame el tamaño :)");
+	JLabel lbx = new JLabel("Widht: ");
+	JLabel lby = new JLabel("Height: ");
+	JButton boton = new JButton("Cambiar tamaño");
 	public Ej1() {
 		//Creamos el panel
-		setTitle("Saludador");
+		setTitle("Ejercicio1");
 		setBounds(600, 500, 400, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
@@ -32,23 +33,29 @@ public class Ej1 extends JFrame {
 		
 		//Asignamos las cardinalidades y el tamaño de los componentes
 		lb.setBounds(50,70,500,20);
-		lb2.setBounds(350,70,500,20);
-		comboBox.setBounds(350,100,150,20);
-		texto.setHorizontalAlignment(SwingConstants.CENTER);//Ponemos el texto en el centro
-		texto.setBounds(50,100,190,20);
+		x.setHorizontalAlignment(SwingConstants.CENTER);//Ponemos el texto en el centro
+		x.setBounds(50,100,190,20);
+		lbx.setBounds(10,100,190,20);
+		y.setHorizontalAlignment(SwingConstants.CENTER);//Ponemos el texto en el centro
+		y.setBounds(50,130,190,20);
+		lby.setBounds(8,130,190,20);
 		boton.setBounds(50,160,190,20);
 		//Lo añadimos al panel
 		panel.add(lb);
-		panel.add(lb2);
-		panel.add(texto);
+		panel.add(x);
+		panel.add(y);
+		panel.add(lbx);
+		panel.add(lby);
 		panel.add(boton);
-		panel.add(comboBox);
+
 		boton.addActionListener(new Resultado());
 	}
 	//Creamos el evento del boton
 	public class Resultado implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			comboBox.addItem(texto.getText());
+			int intx = Integer.parseInt(x.getText());
+			int inty = Integer.parseInt(y.getText());
+			lb.setBounds(50,70,intx,inty);
 		}
 		
 	}
